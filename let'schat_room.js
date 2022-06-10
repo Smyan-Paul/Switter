71449ab1b4ab4d9dfaa8b544ff3af6a5df4959cb
Smyan-Paul <87228812+Smyan-Paul@users.noreply.github.com>
2022-06-10 17:20:13 +0530

    Add files via upload

--- let'schat_room.js
+++ let'schat_room.js
@@ -1,18 +1,40 @@
 var firebaseConfig = {
-      apiKey: "AIzaSyAMGS4rk4XFCLWkSFgG1GbI-nxRytCkCFc",
-      authDomain: "kwitter-bc622.firebaseapp.com",
-      projectId: "kwitter-bc622",
-      storageBucket: "kwitter-bc622.appspot.com",
-      messagingSenderId: "81355538959",
-      appId: "1:81355538959:web:514a83bf8248c17ae8597a"
+      apiKey: "AIzaSyAoFzX0vBh1Z2Xff8gW72IJ4KU3d8zSD5M",
+      authDomain: "switter-c3659.firebaseapp.com",
+      databaseURL: "https://switter-c3659-default-rtdb.firebaseio.com",
+      projectId: "switter-c3659",
+      storageBucket: "switter-c3659.appspot.com",
+      messagingSenderId: "241608588508",
+      appId: "1:241608588508:web:2f69703e9a6d0de47e0819"
     };
     
     firebase.initializeApp(firebaseConfig);
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });});}
-getData();
+    user_name = localStorage.getItem("user_name");
+    document.getElementById("user_name").innerHTML="Welcome " + user_name + " !";
+    
+    function addRoom()
+    {
+          room_name = document.getElementById('room_name').value;
+          firebase.database().ref("/").child(room_name).update({
+                purpose : "adding room name"
+          });
+          localStorage.setItem("room_name" , room_name);
+          window.location = "switter_page.html";
+    }
+    
+    function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+           Room_names = childKey;
+          
+           console.log("room name - " + Room_names);
+           row = "<div class = 'room_name' id = " + Room_names + " onclick = 'redirectToRoomName(this.id)'>#" + Room_names + "</div> <hr>";
+           document.getElementById("output").innerHTML += row;
+          });});}
+    getData();
+    
+    function redirectToRoomName(name)
+    {
+          console.log(name);
+          localStorage.setItem("room_name", name);
+          window.location = "switter_page.html";
+    }
